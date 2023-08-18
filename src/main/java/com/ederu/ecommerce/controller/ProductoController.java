26f56e9a78d8f9432885fbbe2dd7a4a7482ab0fa
gonzalezei <gonzalezei@globalhitss.com>
2023-08-17 20:06:59 -0600

    Funcionalidad guardar producto,  vista crear, logger para la clase ProductoController y funcion para obtener la fecha con javascript

--- src/main/java/com/ederu/ecommerce/controller/ProductoController.java
+++ src/main/java/com/ederu/ecommerce/controller/ProductoController.java
@@ -1,15 +1,42 @@
 package com.ederu.ecommerce.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.ederu.ecommerce.model.Producto;
+import com.ederu.ecommerce.model.Usuario;
+import com.ederu.ecommerce.service.ProductoService;
 
 @Controller
 @RequestMapping("/productos")
 public class ProductoController {
 
+	private final Logger LOOGER= LoggerFactory.getLogger(ProductoController.class);
+	
+	@Autowired
+	private ProductoService productoService;
+	
 	@GetMapping("")
 	public String show() {
 		return "productos/show";
+	}
+	
+	@GetMapping("/create")
+	public String create() {
+		return "productos/create";
+	}
+	
+	@PostMapping("/save")
+	public String save(Producto producto) {
+		LOOGER.info("Este es el objeto producto {}", producto );
+		Usuario u = new Usuario(1, "", "", "", "", "", "", "");
+		producto.setUsuario(u);
+		productoService.save(producto);
+		return "redirect:/productos";
 	}
 }
---
+++ src/main/resources/static/js/functions.js
@@ -0,0 +1,11 @@
+//function fecha() {
+//		let to_day = new Date();
+//		let year = to_day.getFullYear();
+//		document.getElementById('year').innerHTML = 'Copyright &copy; Spring eCommerce ' + year;
+//	}; 
+
+(function () {
+			let to_day = new Date();
+			let year = to_day.getFullYear();
+			document.getElementById('year').innerHTML = 'Copyright &copy; Spring eCommerce ' + year;
+		})();
